b4fd29c6de9c1d59b843875b28c4029df878fe14
David B. Waters <mrdavidbwaters@gmail.com>
2020-12-10 21:08:32 -0500

    fix typo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -881,7 +881,7 @@ render(
                     </p>
                     <p>
                       I also helped the team refine the
-                      React-based frontend durning a sprint
+                      React-based frontend during a sprint
                       before a deadline.
                     </p>
                   </article>
